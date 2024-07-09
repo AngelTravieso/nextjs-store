0abced70a232de604901cc086c57f5918e90bc43
AngelTravieso <angeltraviesoc@gmail.com>
2024-07-09 00:11:48 -0400

    React Server Components en Next.js: notacion 'use client'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+// "use client";
+
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  console.log('Hola Mundo pagina de inicio');
+
   return (
     <main className={styles.main}>
       <h1>Hola Mundo</h1>

bf6ad0b92a75c713a038a58233f1b2b1b078f21b
AngelTravieso <angeltraviesoc@gmail.com>
2024-07-10 15:52:59 -0400

    Como utilizar estilos globales en Next.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import { Inter } from "next/font/google";
 import { Header } from "app/components/shared/Header";
 import { Footer } from "app/components/shared/Footer";
 
+// Importar estilos globales de SASS
+import "app/sass/globals.sass";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({

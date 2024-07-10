c0ed652a7c4701740d5b98b3e7a5e8706f5ab1c5
AngelTravieso <angeltraviesoc@gmail.com>
2024-07-10 15:25:20 -0400

    Uso de Sass en Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Convert the import.meta.url to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/sass')],
+    prependData: `@import "main.sass"`,
+  },
+};
 
 export default nextConfig;

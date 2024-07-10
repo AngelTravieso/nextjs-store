6b46bf378dce52e466cd5334cf92bde97cbdf76e
AngelTravieso <angeltraviesoc@gmail.com>
2024-07-10 14:29:20 -0400

    CSS Modules en Next.js 13

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,7 +1,13 @@
+import styles from "./Hero.module.css";
+
 export const Hero = () => {
+  console.log(styles);
+
   return (
-    <section>
-        <h1>Hero</h1>
+    // Aplicar el estilo del Hero.module
+    <section className={styles.Hero}>
+      <h1>Future world</h1>
+      <h2>Empowering Yout Tomorrow, Today!</h2>
     </section>
-  )
-}
+  );
+};

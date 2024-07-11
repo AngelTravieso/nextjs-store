f41d66fb4c86544c727f2a77536cac64e66e281d
AngelTravieso <angeltraviesoc@gmail.com>
2024-07-11 13:24:49 -0400

    Manejo de variables de entorno en Next.js

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 export const MainProducts = () => {
-    return (
-        <section>
-            <h1>MainProducts</h1>
-        </section>
-    )
-}
+  // Obtener valor del .env
+  console.log(process.env.NEXT_PUBLIC_SHOPIFY_HOSTNAME);
+  // console.log(process.env.SHOPIFY_API_KEY);
+
+  return (
+    <section>
+      <h1>Main Products</h1>
+    </section>
+  );
+};

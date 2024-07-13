af60781fc4749578a7608b94db3b86d66d122371
AngelTravieso <angeltraviesoc@gmail.com>
2024-07-13 15:20:19 -0400

    Implementado la pagina de tienda

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,3 +1,6 @@
+import { ProductsWrapper } from "app/components/store/ProductsWrapper";
+import { getProducts } from '../../../services/shopify/index';
+
 interface CategoryProps {
   params: {
     categories: string[];
@@ -5,13 +8,19 @@ interface CategoryProps {
   };
 }
 
-export default function Category(props: CategoryProps) {
+export default async function Category(props: CategoryProps) {
   // console.log(props)
+
+  const products = await getProducts()
 
   const { categories } = props.params;
 
   // Simular error para mostrar la página de error
   // throw new Error('Error: Boom');
 
-  return <h1>Categoria dinamica: {categories}</h1>;
+  // return <h1>Categoria dinamica: {categories}</h1>;
+
+  return (
+    <ProductsWrapper products={products} />
+  )
 }
---
+++ src/components/store/ProductsWrapper/ProductsWrapper.tsx
@@ -0,0 +1,17 @@
+import { Product } from "app/interfaces/products"
+import { ProductCard } from "../ProductCard"
+import styles from './ProductsWrapper.module.sass'
+
+interface ProductsWrapperProps {
+    products: Product[]
+}
+
+export const ProductsWrapper = ({ products }: ProductsWrapperProps) => {
+    return (
+        <div className={styles.ProductsWrapper}>
+            {products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+            ))}
+        </div>
+    )
+}
